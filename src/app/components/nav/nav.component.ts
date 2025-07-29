3a20bde3e9767ff738c206ca79cebce2e158bcbd
rafaelTabasca <rafael@capicua.com.uy>
2025-07-29 11:16:03 -0400

    improved details

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,7 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
 import { CounterStore } from '../../store/counter/counter.store';
 
 @Component({
@@ -12,11 +9,6 @@ import { CounterStore } from '../../store/counter/counter.store';
 })
 
 export class NavComponent {
-  private breakpointObserver = inject(BreakpointObserver);
   counterStore = inject(CounterStore);
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+ 
 }
